fad9d9ca924eb59de771c272bb70f248d6d0467f
Aline <alinedeoliveira2012@gmail.com>
2018-03-24 14:13:19 -0300

    mediana ok

--- exercicio3.py
+++ exercicio3.py
@@ -39,7 +39,7 @@ def filter_average(tam_filter,matriz_aux,img_size):
 			soma=0
 			for i_kernel in range (0,tam_filter):
 				for j_kernel in range (0, tam_filter):
-					
+				
 					aux=matriz_aux[i_mat+i_kernel,j_mat+j_kernel]*kernel[i_kernel,j_kernel]
 					soma=soma+aux
 
@@ -47,9 +47,25 @@ def filter_average(tam_filter,matriz_aux,img_size):
 
 	return img_filtered
 
+def filter_median (tam_filter,matriz_aux,img_size):
+
+	size_matriz=matriz_aux.shape
+	img_filtered2=np.zeros((img_size[0],img_size[1],3), dtype=np.uint8)
+
+	for k_mat in range (0,3):
+		print k_mat
+		for i_mat in range(0,size_matriz[0]-tam_filter+1):
+			for j_mat in range(0,size_matriz[1]-tam_filter+1):
+
+				aux= matriz_aux[i_mat:i_mat+tam_filter,j_mat:j_mat+tam_filter,k_mat]
+				aux = np.median(aux)
+
+				img_filtered2[i_mat,j_mat,k_mat]=aux
+	return img_filtered2
+
 #---------------------------------------------------------------------------------------------------------
 
-arq = open('filtro9.txt', 'r')
+arq = open('filtro7.txt', 'r')
 text = arq.read()
 print(text)
 arq.close()
@@ -68,6 +84,9 @@ img_filtered=filter_average(tam,matriz_result,img_size)
 plt.imshow(img_filtered,origin='upper')
 plt.show()
 
+img_filtered_median=filter_median(tam,matriz_result,img_size)
+plt.imshow(img_filtered_median)
+plt.show()
 
 
 
